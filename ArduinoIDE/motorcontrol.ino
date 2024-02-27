44a95651f94b30df7e03de6b186aa8b9e09a5191
Luke Edgecombe <145694364+Luk446@users.noreply.github.com>
2024-02-27 16:15:06 +0000

    Add files via upload

--- ArduinoIDE/motorcontrol.ino
+++ ArduinoIDE/motorcontrol.ino
@@ -3,6 +3,9 @@
     Created by Luke Edgecombe (2450027)
     Syntax derived from the arduiniogetstarted.com site
 
+    Code can be utilised in order to understand the cycle limits 
+    with construction
+
 */
 
 // init motor control pins from the shield
@@ -15,7 +18,7 @@ const int IN = 2;
 const int OUT = 3;
 
 void setup() {
-  
+ 
 // Init the previously declared pins into a certain type
   pinMode(ENA, OUTPUT);
   pinMode(IN, OUTPUT);
@@ -27,8 +30,8 @@ digitalWrite(ENA, HIGH);
 }
 
 void loop() {
-// basic test loops infinite 
-
+// basic test loops loops for 200 iterations 
+  for(int i = 0; i<=200; i++){
 // extend motor
 
   digitalWrite(IN, LOW);
@@ -43,5 +46,7 @@ void loop() {
 
 //wait
   delay(20000);
+
+  }
 }
 
